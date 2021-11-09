9c30de893f103664974b174448e678ee934c97f6
Charles Becker <c.l.becker@outlook.com>
2021-11-09 13:50:17 -0800

    today

--- main.R
+++ main.R
@@ -106,3 +106,111 @@ data.frame(R) %>% ggplot(aes(sample=R)) +
                   stat_qq() +
                   geom_abline( intercept=mean(R), slope = sqrt((1-mean(R)^2/N-2)))
 
+# Anscombe's quartet ================================
+rm(list=ls())
+library(tidyverse)
+library(HistData)
+data("GaltonFamilies")
+
+set.seed(1983)
+
+galton_heights <- GaltonFamilies %>%
+  filter( gender == "male" ) %>%
+  group_by( family ) %>%
+  sample_n(1) %>%
+  ungroup() %>%
+  select( father, childHeight ) %>%
+  rename( son = childHeight )
+
+conditional_avg <- galton_heights %>%
+  filter( round( father ) == 72 )%>%
+  summarize( avg = mean( son ) ) %>%
+  .$avg
+conditional_avg
+
+sum( galton_heights$father == 72 )
+sum( galton_heights$father == 72.5 )
+
+
+conditional_avg <- galton_heights %>%
+  filter(round(father) == 72) %>%
+  summarize(avg = mean(son)) %>%
+  pull(avg)
+conditional_avg
+
+galton_heights %>%
+  mutate( father_strata = factor( round( father ) ) ) %>%
+  ggplot( aes( father_strata, son ) ) +
+  geom_boxplot() +
+  geom_point()
+
+galton_heights %>%
+  mutate( father = round( father ) ) %>% 
+  group_by( father ) %>%
+  summarize( son_conditional_avg = mean( son ) ) %>%
+  ggplot( aes( father, son_conditional_avg ) ) +
+  geom_point()
+
+r <- galton_heights%>% summarize(r=cor(father, son)) %>% .$r
+  
+galton_heights %>%
+   mutate(father = round(father)) %>%
+   group_by(father) %>%
+   summarize(son = mean(son)) %>%
+   mutate(z_father = scale(father), z_son = scale(son)) %>%
+   ggplot(aes(z_father, z_son)) +
+     geom_point() +
+     geom_abline(intercept=0, slope = r)
+
+
+# rosetta stone for all of this that follows >>>>>>>>>
+# slope     <- correlation * sd_y / sd_x
+# intercept <- mean_y - slope * mean_x
+
+mu_x <- mean(galton_heights$father)
+mu_y <- mean(galton_heights$son)
+s_x <- sd(galton_heights$father)
+s_y <- sd(galton_heights$son)
+r <- cor(galton_heights$father, galton_heights$son)
+m <- r * s_y/s_x
+b <- mu_y - m*mu_x
+
+galton_heights %>%
+  ggplot(aes(scale(father), scale(son))) +
+  geom_point(alpha = 0.5) +
+  geom_abline(intercept = 0, slope = r)
+
+#Bivariate Normal Distribution
+
+galton_heights %>%
+     mutate(z_father = round( (father - mean(father))/sd(father))) %>%
+     filter(z_father %in% -2:2) %>%
+     ggplot() +
+       stat_qq(aes(sample=son)) +
+       facet_wrap(~z_father)
+
+
+# 11/9/2021
+rm(list=ls())
+library(tidyverse)
+
+set.seed(1989, sample.kind="Rounding")
+library(HistData)
+data("GaltonFamilies")
+
+female_heights <- GaltonFamilies %>%
+                  filter( gender == "female" ) %>%
+                  group_by( family ) %>%
+                  sample_n(1) %>%
+                  ungroup() %>%
+                  select( mother, childHeight ) %>%
+                  rename( daughter = childHeight )
+mother_mean <- mean(female_heights$mother)
+mother_sd <- sd( female_heights$mother )
+
+daughter_mean <- mean(female_heights$daughter)
+daughter_sd <- sd( female_heights$daughter )
+modau_cor <- cor( female_heights$mother, female_heights$daughter)
+
+moda_slope <- modau_cor * ( daughter_sd/mother_sd )
+moda_intercept <- daughter_mean - ( moda_slope * mother_mean )
