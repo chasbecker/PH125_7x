80a2ae2335890a7f7d88105716d103f4af924bc2
Charles Becker <c.l.becker@outlook.com>
2021-11-12 12:49:44 -0800

    zz

--- main.R
+++ main.R
@@ -223,3 +223,62 @@ expected_daughter <- moda_intercept + ( if_mother * moda_slope )
 # 11/10/2021 Linear Models Overview
 rm(list=ls())
 library(tidyverse)
+library(Lahman)
+# View(Teams)
+
+bb_slope <- Teams %>%
+           filter( yearID %in% 1961:2001 ) %>%
+           mutate( BB_per_game = BB/G, R_per_game = R/G ) %>%
+           lm( R_per_game ~ BB_per_game, data = . ) %>%
+           .$coef %>%
+           .[2]
+bb_slope
+
+singles_slope <- Teams %>%
+                 filter( yearID %in% 1961:2001 ) %>%
+                 mutate( Singles_per_game = (H-HR-X2B-X3B)/G, R_per_game = R/G ) %>%
+                 lm( R_per_game ~ Singles_per_game, data = . ) %>%
+                 .$coef %>%
+                 .[2]
+singles_slope
+
+Teams %>%
+  filter( yearID %in% 1961:2001 ) %>%
+  mutate( Singles = (H-HR-X2B-X3B)/G, BB = BB/G, HR = HR/G ) %>%
+  summarize( cor(BB, HR), cor(Singles, HR), cor(BB, Singles) )
+
+dat <- Teams %>%
+       filter( yearID %in% 1961:2001 ) %>%
+       mutate( HR_strata = round( HR/G, 1),
+               BB_per_game = BB/G,
+               R_per_game = R/G ) %>%
+       filter( HR_strata >= 0.4 & HR_strata <= 1.2 )
+
+hr_strat_plot <- dat %>% ggplot( aes(BB_per_game, R_per_game) ) +
+        geom_point( alpha = 0.5 ) +
+        geom_smooth( method = "lm" ) +
+        facet_wrap( ~ HR_strata )
+hr_strat_plot
+
+dat %>% group_by( HR_strata ) %>%
+        summarize( slope = cor(BB_per_game, R_per_game )*(sd(R_per_game)/sd(BB_per_game)))
+
+###===========================================================
+dat <- Teams %>%
+        filter( yearID %in% 1961:2001 ) %>%
+        mutate( BB_strata = round(BB/G, 1),
+                HR_per_game = HR/G,
+                R_per_game = R/G ) %>%
+        filter( BB_strata >= 2.8 & BB_strata <= 3.9 )
+
+lm_plot <- dat %>% ggplot( aes( HR_per_game, R_per_game ) ) +
+         geom_point( alpha = 0.5 ) +
+         geom_smooth( method = "lm" ) +
+         facet_wrap( ~ BB_strata )
+lm_plot
+
+dat %>% group_by(BB_strata) %>%
+        summarize( slope = cor(HR_per_game, R_per_game)*(sd(R_per_game)/sd(HR_per_game)))
+###===========================================================
+
+
