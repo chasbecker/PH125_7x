7fa21e362843a235166ee581f4bfaa240342ed88
Charles Becker <c.l.becker@outlook.com>
2021-11-18 11:27:53 -0800

    completed 2.2 LSE Q3

--- main.R
+++ main.R
@@ -264,6 +264,7 @@ dat %>% group_by( HR_strata ) %>%
         summarize( slope = cor(BB_per_game, R_per_game )*(sd(R_per_game)/sd(BB_per_game)))
 
 ###===========================================================
+
 dat <- Teams %>%
         filter( yearID %in% 1961:2001 ) %>%
         mutate( BB_strata = round(BB/G, 1),
@@ -279,6 +280,119 @@ lm_plot
 
 dat %>% group_by(BB_strata) %>%
         summarize( slope = cor(HR_per_game, R_per_game)*(sd(R_per_game)/sd(HR_per_game)))
-###===========================================================
+### 2.2 ==========================
 
+rm(list=ls())
+library(tidyverse)
+library(HistData)
+data("GaltonFamilies")
+set.seed(1983)
+galton_heights <- GaltonFamilies %>%
+                    filter(gender == "male" ) %>%
+                    group_by(family) %>%
+                    sample_n(1) %>%
+                    ungroup() %>%
+                    select(father, childHeight) %>%
+                    rename( son = childHeight )
+
+rss <- function(beta0, beta1, data){
+  resid <-  galton_heights$son - (beta0 + beta1*galton_heights$father)
+  return( sum(resid^2) )
+  }
+
+beta1 = seq(0, 1, len=nrow(galton_heights))
+results <-  data.frame(beta1 = beta1,
+                       rss = sapply(beta1, rss, beta0 = 25))
+
+
+results %>% ggplot(aes(beta1, rss)) +
+              geom_line() +
+              geom_line(aes(beta1, rss))
+
+fit <- lm( son ~ father, data = galton_heights )
+fit
+
+summary(fit)
+
+B <- 1000
+N <- 50
+
+lse <- replicate( B, { sample_n( galton_heights, N, replace = TRUE )%>%
+                       lm( son ~ father, data = .) %>%
+                      .$coef 
+                     }
+                 )
+
+lse <- data.frame( beta_0 = lse[1,], beta_1 = lse[2,])
+
+library(gridExtra)
+p1 <- lse %>% ggplot(aes(beta_0)) + geom_histogram(binwidth = 5, color = "black")
+p2 <- lse %>% ggplot(aes(beta_1)) + geom_histogram(binwidth=0.1, color = "black")
+grid.arrange(p1,p2,ncol=2)
+
+sample_n(galton_heights, N, replace = TRUE) %>%
+  lm(son ~ father, data = .) %>%
+  summary %>%
+  .$coef
+
+lse %>% summarize( se_0 = sd(beta_0), se_1 = sd(beta_1))
+
+
+lse %>% summarize(cor(beta_0, beta_1))
+
+B <- 1000
+N <- 50
+lse <- replicate( B, {sample_n(galton_heights, N, replace = TRUE) %>%
+                                 mutate( father = father - mean(father)) %>%
+                                 lm( son ~ father, data = .) %>%
+                                 .$coef
+                      }
+                )
+
+cor(lse[1,], lse[2,])
+
+galton_heights %>% ggplot(aes(father, son)) +
+                     geom_point() +
+                     geom_smooth(method="lm")
+
+fit <- galton_heights %>%
+         lm(son ~ father, data = .)
+
+Y_hat <- predict(fit, se.fit = TRUE)
+names(Y_hat)
+galton_heights %>%
+        mutate(Y_hat = predict(lm(son~father, data = .))) %>%
+        ggplot(aes(father, Y_hat)) +
+          geom_line()
+
+#++++++++++++++++++++++++++++
+beta1 = seq(0, 1, len=nrow(galton_heights))
+results <-  data.frame(beta1 = beta1,
+                       rss = sapply(beta1, rss, beta0 = 36))
+
+
+results %>% ggplot(aes(beta1, rss)) +
+  geom_line() +
+  geom_line(aes(beta1, rss))
+
+# LSE assessentm Q3 ++++++++++++++++++++++++++++++++++++++
+
+rm(list=ls())
+library(tidyverse)
+library(Lahman)
+
+# View(Teams)
+str(Teams)
+
+p1 <- Teams %>%
+        filter( yearID %in% 1961:2001 )
+
+rg <- p1$R / p1$G
+bbg <- p1$BB / p1$G
+hrg <- p1$HR / p1$G
+
+reg_p1 <- lm( rg ~ bbg + hrg )
+summary( reg_p1 )
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
